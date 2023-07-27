2dce1af3ef5d3df7c26c961092a600e5248c6fcb
Sumeya <sumeyadahir8@gmail.com>
2023-07-27 11:18:17 +0100

    Initial Commit

---
+++ user/userApp.swift
@@ -0,0 +1,17 @@
+//
+//  userApp.swift
+//  user
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct userApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
