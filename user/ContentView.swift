160ec8ae7a67fa44c68a5d64e4e893003687ca5a
Sumeya <sumeyadahir8@gmail.com>
2023-08-01 09:36:33 +0100

    comment

--- user/ContentView.swift
+++ user/ContentView.swift
@@ -8,19 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var isClicked = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        //comment
+        
+        ZStack {
+            Color(.systemPink)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0) {
+                
+                
+                
+                VStack {
+                    
+                    Text("all about me x ")
+                        .font(.title)
+                    
+                    Image("susan")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    
+                    
+                    
+                    Button("continue reading here") {
+                        isClicked = true
+                    }
+                    .padding(.all)
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.black)
+                    .padding(.horizontal)
+                    .background(Rectangle()
+                        .foregroundColor(.white))
+                    .padding()
+                    .cornerRadius(60.0)
+                    .shadow(radius: 50)
+                    
+                    
+                    
+                
+                    if isClicked == true{
+                        Text("I am a 15 yo girl from London, I love baking and reading and coding!! This will be my second app ever.")
+                        
+                    }
+                    
+                    
+                    
+                }
+                
+            }
         }
-        .padding()
+                }
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    
 }
